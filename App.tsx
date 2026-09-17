42f3701f0792c077054ae7d52bcbc0a85a5e32c0
Isaac NSB Kargbo <isaacnsbkargbo20@gmail.com>
2026-09-17 23:15:06 +0100

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,45 +1,76 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import { useEffect } from "react"
+import { StyleSheet, Text, View } from "react-native"
+import { Camera, useCameraDevice, useCameraPermission } from "react-native-vision-camera"
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+function App(): React.JSX.Element {
+  const device = useCameraDevice("back")
+  const { hasPermission, requestPermission } = useCameraPermission()
 
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+  useEffect(() => {
+    if (!hasPermission) {
+      requestPermission()
+    }
+  }, [hasPermission, requestPermission])
 
-  return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
+  if (!hasPermission) {
+    return (
+      <View style={styles.permissionContainer}>
+        <Text style={styles.title}>Camera permission is required</Text>
+        <Text style={styles.message}>VisionAid needs access to your camera to detect objects and obstacles.</Text>
+      </View>
+    )
+  }
 
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
+  if (device === undefined) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Camera not available</Text>
+        <Text style={styles.message}>VisionAid could not find a camera on this device</Text>
+      </View>
+    )
+  }
 
   return (
     <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
+      <Camera
+        style={StyleSheet.absoluteFill}
+        device={device}
+        isActive={true}
       />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'black'
   },
-});
+
+  permissionContainer: {
+    flex: 1,
+    backgroundColor: 'black',
+    paddingHorizontal: 24,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  title: {
+    color: 'white',
+    fontSize: 26,
+    fontWeight: 700,
+    textAlign: 'center',
+    marginBottom: 16,
+    flexShrink: 1,
+  },
+
+  message: {
+    color: 'white',
+    fontSize: 18,
+    lineHeight: 27,
+    textAlign: 'center',
+    flexShrink: 1
+  },
+})
 
 export default App;
